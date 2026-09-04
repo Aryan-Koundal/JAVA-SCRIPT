1321852d9b681f6c63520b5182893192c8604a87
Aryan-Koundal <aryankoundal3039@gmail.com>
2026-09-04 22:42:19 +0530

    learning java script

---
+++ intro.js
@@ -0,0 +1,26 @@
+console.log("Welcome to Aryan Koundal's world");
+fullname = "ARYAN KOUNDAL";
+console.log(fullname);
+let age = 18;
+age = 19;
+const name = "Aryan"
+console.log(age);
+console.log(name)
+
+{
+    let age = 18;
+    console.log(age)
+}
+{
+    let age =19;
+    console.log(age)
+}
+let x = BigInt("1234");
+let y = Symbol("hello");
+
+let student = {
+    fullname :"Aryan koundal",
+    age : 18,
+    cgpa : 9.0
+};
+student.age = student.age + 1;
